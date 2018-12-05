663e10635a3ea0d8f92003aa257ec6d5c03c8e82
Judy Gonzalez <judy@thearrayschool.com>
2018-12-05 16:15:47 -0700

    final

--- answer.js
+++ answer.js
@@ -1,1 +1,19 @@
-// *** YOUR ANSWER BELOW ***
+// *** YOUR ANSWER BELOW **
+
+function negate(x){
+  return "un" + x;
+
+}
+
+function intensify(x){
+  return "plus" + (x);
+
+
+}
+
+function reinforce(x){
+  return "double" + x;
+
+}
+
+reinforce(intensify("cold"))
